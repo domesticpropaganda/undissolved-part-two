fa2fd057dcacca90570cae5e3665855fc54679e2
erdalsson <76843358+domesticpropaganda@users.noreply.github.com>
2025-07-20 23:49:33 +0200

    file location update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/unfashioned/', // Set base for GitHub Pages deployment
+  base: '/undissolved-part-two/', // Set base for GitHub Pages deployment
   // Add Vite options here if needed
 });
